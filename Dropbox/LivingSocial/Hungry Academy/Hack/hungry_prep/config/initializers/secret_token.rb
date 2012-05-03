942abd8b4039d00ff2f2a6704e836894832074a6
Chris Maddox <tyre77@gmail.com>
2012-05-03 14:32:15 -0400

    init

---
+++ Dropbox/LivingSocial/Hungry Academy/Hack/hungry_prep/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HungryPrep::Application.config.secret_token = 'bb61a15a55f61479e7b957a94fa784935eb4ca569f396f6b3740492f9537ddd9b29c47d83049c14924118a0f7b15936aa007bcfb6b5ce3e18fb1697b218f3a06'
